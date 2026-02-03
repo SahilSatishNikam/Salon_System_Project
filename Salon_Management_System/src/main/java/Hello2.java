7522d238e8e2566549790d161f3520269b941b8f
nayanamali <malinayana23@gmail.com>
2026-02-03 19:23:44 +0530

    hello2
    hello2

---
+++ Salon_Management_System/src/main/java/Hello2.java
@@ -0,0 +1,9 @@
+
+public class Hello2 {
+   
+	   public static void main(String args[])
+	   {
+		      System.out.println("Hello anand");
+		      System.out.println("Hello World andn ");
+	   }
+}
