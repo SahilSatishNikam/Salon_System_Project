62688efd9679a3996da236d2fbe68ba37145b415
nayanamali <nayanamali605@gmail.com>
2026-02-03 19:41:49 +0530

    ttt
    ggg

--- Salon_Management_System/src/main/java/Hello2.java
+++ Salon_Management_System/src/main/java/Hello2.java
@@ -3,7 +3,7 @@ public class Hello2 {
    
 	   public static void main(String args[])
 	   {
-		      System.out.println("Hello anand");
+		      System.out.println("Hello anand hhh");
 		      System.out.println("Hello World andn ");
 	   }
 }
